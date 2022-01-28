369a6cf6de1d9e6be1022bf94048686f1ddfb679
DesktopCoder1407 <dcoder1407@gmail.com>
2022-01-28 17:06:47 -0500

    Begin Problem 4

--- Assignment1/src/AnagramsCheck.java
+++ Assignment1/src/AnagramsCheck.java
@@ -6,28 +6,39 @@
 
 /* -----Algorithm Design Block-----
  * 
- * Problem 3: Design an algorithm to determine whether two strings are the same.
+ * Problem 4: Design an algorithm to determine whether two strings are anagrams.
  * 
- * First, the algorithm would check to see whether the lengths of the two strings
- * are the same. If not, the two strings cannot be the same. If so, a loop could
- * iterate through each character of the first string and compare it to the same
- * character index in the second string. If all the characters are the same, the
- * strings are equal.
+ * First, the algorithm must remove all spaces from both input strings. Then, 
+ * the algorithm would compare whether the two modified input strings are the 
+ * same length. If they aren't, the strings cannot be anagrams. Otherwise, create
+ * a loop that iterates through each character in the first string. Within the 
+ * outer loop is an inner loop that iterates through each character in the second 
+ * string. If the outer loop's character equals the inner loop's character, remove
+ * the character from the second string and continue to the next iteration in the
+ * outer loop. If the inner loop cannot find a character that matches the outer
+ * loop's character, the two strings are not anagrams. If the outer loop completes,
+ * all characters match, therefore the two strings are anagrams.
  * 
  * Pseudocode:
  * //s1 is string 1
  * //s2 is string 2
  * 
+ * s1 <- s1.removeSpaces
+ * s2 <- s2.removeSpaces
  * if s1.length != s2.length then
  *   return false
  * for i <- 0 to s1.length - 1 do
- *   if s1[i] != s2[i] then
- *     return false
+ *   for j <- 0 to s2.length - 1 do
+ *     if s1[i] == s2[j]
+ *       s2 <- s2.removeFirstCharacter(j)
+ *       break
+ *   //Strings are not anagrams if the program reaches this point
+ *   return false
  * return true
  */
 import java.util.Scanner;
 
-public class StringMatching {
+public class AnagramsCheck {
 	static int comparisons = 0;
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -53,7 +64,7 @@ public class StringMatching {
 			else if (input == 2) { //Runs the algorithm and displays results upon receiving a 2
 				System.out.println("\nString 1: " + string1);
 				System.out.println("String 2: " + string2);
-				System.out.println("Output: " + isStringMatching(string1, string2));
+				System.out.println("Output: " + isAnagram(string1, string2));
 				System.out.println("Comparisons: " + comparisons);
 			}
 			else if (input == 1) { //Reads two input strings upon receiving a 1
@@ -65,20 +76,8 @@ public class StringMatching {
 		}
 		scan.close();
 	}
-	
-	static String isStringMatching(String string1, String string2) {
-		comparisons = 0; //Resets number of comparisons to zero.
-		
-		comparisons++; //Increments the number of comparisons by one due to checking whether the two strings are of equal length.
-		//If the strings are not of equal length, they cannot match.
-		if(string1.length() != string2.length())
-			return "Strings do not match";
-		//For each character in both strings, check to see whether they match. If they don't, the strings don't match and the loop can be terminated.
-		for(int i = 0; i < string1.length(); i++) {
-			comparisons++; //Increment comparison counter before each comparison
-			if(string1.charAt(i) != string2.charAt(i))
-				return "Strings do not match";
-		}
-		return "Strings match"; //Only reaches here if the strings are of the same length and all characters match.
+
+	static String isAnagram(String string1, String string2) {
+		return "Strings are anagrams";
 	}
 }
