74b5853977d9a5c629344f02770960f4cedf4a43
DesktopCoder1407 <dcoder1407@gmail.com>
2022-01-28 20:52:31 -0500

    Complete Problem 4

--- Assignment1/src/AnagramsCheck.java
+++ Assignment1/src/AnagramsCheck.java
@@ -8,32 +8,27 @@
  * 
  * Problem 4: Design an algorithm to determine whether two strings are anagrams.
  * 
- * First, the algorithm must remove all spaces from both input strings. Then, 
- * the algorithm would compare whether the two modified input strings are the 
- * same length. If they aren't, the strings cannot be anagrams. Otherwise, create
- * a loop that iterates through each character in the first string. Within the 
- * outer loop is an inner loop that iterates through each character in the second 
- * string. If the outer loop's character equals the inner loop's character, remove
- * the character from the second string and continue to the next iteration in the
- * outer loop. If the inner loop cannot find a character that matches the outer
- * loop's character, the two strings are not anagrams. If the outer loop completes,
+ * First, the algorithm must change all inputs to a single case and ignore spaces,
+ * since anagrams can move captials and spaces freely. Then, it must loop through
+ * each character in the first string. Within the outer loop is an inner loop
+ * that iterates through each character in the second string.
+ * If the outer loop's character equals the inner loop's character, remove the
+ * character in the second string and continue to the next iteration in the outer loop.
+ * If the inner loop cannot find a character that matches the outer loop's
+ * character, the two strings are not anagrams. If the outer loop completes,
  * all characters match, therefore the two strings are anagrams.
  * 
  * Pseudocode:
- * //s1 is string 1
- * //s2 is string 2
+ * //s1 is string 1 in a single case and with spaces removed
+ * //s2 is string 2 in a single case and with spaces removed
  * 
- * s1 <- s1.removeSpaces
- * s2 <- s2.removeSpaces
- * if s1.length != s2.length then
- *   return false
  * for i <- 0 to s1.length - 1 do
  *   for j <- 0 to s2.length - 1 do
  *     if s1[i] == s2[j]
- *       s2 <- s2.removeFirstCharacter(j)
+ *       s2[j] <- ""
  *       break
- *   //Strings are not anagrams if the program reaches this point
- *   return false
+ *     if j == s2.length - 1
+ *       return false //Only activates if the if statement is false AND this is the last iteration of the inner loop
  * return true
  */
 import java.util.Scanner;
@@ -78,6 +73,23 @@ public class AnagramsCheck {
 	}
 
 	static String isAnagram(String string1, String string2) {
-		return "Strings are anagrams";
+		comparisons = 0; //Resets comparisons
+		//Removes all spaces and changes all letters to lowercase. Anagrams do not care about case or spaces.
+		string1 = string1.toLowerCase().replaceAll(" ", "");
+		string2 = string2.toLowerCase().replaceAll(" ", "");
+		
+		for(int i = 0; i < string1.length(); i++) { //Outer Loop
+			for(int j = 0; j < string2.length(); j++) { //Inner Loop, has a variable loop length as the length of string2 changes if a matching letter is found.
+				comparisons++;
+				if(string1.charAt(i) == string2.charAt(j)) { //Core comparison
+					//If string1 and string2 have a character in common, remove that character from string2 and continue iterating through string1
+					string2 = string2.replaceFirst(string2.charAt(j) + "", "");
+					break;
+				}
+				if(j+1 == string2.length()) //If the inner loop has completed a full cycle and no character has been found to be in common, the two strings cannot be anagrams.
+					return "Strings are not anagrams";
+			}
+		}
+		return "Strings are anagrams"; //Outer loop complete. Strings are anagrams
 	}
 }
