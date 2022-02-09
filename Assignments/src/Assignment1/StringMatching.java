788ecacf6c25b37f0b1d7516494b9fcc12bd8c83
DesktopCoder1407 <dcoder1407@gmail.com>
2022-02-08 22:59:56 -0500

    Organize and Begin Assignment2

--- Assignments/src/Assignment1/StringMatching.java
+++ Assignments/src/Assignment1/StringMatching.java
@@ -1,3 +1,4 @@
+package Assignment1;
 // Name:		Parker Smith
 // Class:		CS 4306/1
 // Term:		Spring 2022
---
+++ Assignments/src/Assignment2/FakeCoins.java
@@ -0,0 +1,32 @@
+package Assignment2;
+//Name:		Parker Smith
+//Class:		CS 4306/1
+//Term:		Spring 2022
+//Instructor:	Dr. Haddad
+//Assignment:	2
+
+/* -----Algorithm Design Block-----
+* 
+* Problem 1: There are n stacks of n identical-looking coins.
+* All of the coins in one of these stacks are counterfeit, while all the coins in the other
+* stacks are genuine. Every genuine coin weighs 10 grams; every fake weighs 11 grams.
+* You have an analytical scale that can determine the exact weight of any number of coins.
+* Design an algorithm to identify the stack with the fake coins.
+* 
+* Pseudocode:
+* 
+* 
+* Performance Analysis:
+* 
+* 
+* Minimum number of weighings:
+* 
+* 
+* Explain:
+* 
+*/
+public class FakeCoins {
+	public static void main(String[] args) {
+		
+	}
+}
