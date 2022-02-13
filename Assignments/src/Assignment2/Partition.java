3e56d83e69b0f61956e6cd657d1131727a9faab0
DesktopCoder1407 <dcoder1407@gmail.com>
2022-02-12 21:39:02 -0500

    Complete Performance Analysis and Pseudocode

--- Assignments/src/Assignment2/Partition.java
+++ Assignments/src/Assignment2/Partition.java
@@ -9,16 +9,70 @@
 * Problem 3: Design an algorithm that, when given n positive integers, partition them into two disjoint subsets with the same sum of their elements.
 * 
 * PSEUDOCODE:
+* //n number of positive integers
+* //A[n] is an array of size n filled with the inputed integers
+* //B[n] is an array of size n tracking our test subset
+* //target is the sum of all input values divided by two
+* sum <- 0 //the sum of all values we are currently tracking
+* c <- 1 //the number of comparisons
 * 
-* PERFORMANCE ANALYSIS:
+* if target % 1 != 0
+*   return that no disjoint subsets with the same sum exist //Math behind this: target = 2x where x equals the sum of one subset, therefore target MUST be even.
+* i <- 0
+* while i < n do
+*   c <- c + 1
+*   sum <- sum + A[i]
+*   c <- c + 1
+*   if sum = target
+*     B[i] <- 1
+*     return two separate subsets, one of all the values from A[n] where B[n] is 1, and one of all the values from A[n] where B[n] is 0.
+*   c <- c + 1
+*   if sum > target
+*     sum <- sum - A[i]
+*     c <- c + 1
+*     if i < n-1
+*       i <- i + 1
+*       continue
+*     foundTracked <- 0
+*     for j <- n-1 to 0 do
+*       c <- c + 1
+*       if B[j] = 1
+*         foundTracked <- 1
+*         B[j] <- 0
+*         sum <- sum - A[j]
+*         i <- j + 1
+*         break
+*     c <- c + 1
+*     if foundTracked = 0
+*       return that no disjoint subsets with the same sum exist
+*     continue
+*   B[i] <- 1
+*   i <- i + 1
+* return that no disjoint subsets with the same sum exist
 * 
+* PERFORMANCE ANALYSIS: //Sorry this looks messy, I really don't know how to do a performance analysis for a large while loop controlled by a variable that is changed inside the while loop.
+* 							The Best Case and Worst Case I got from placing markers in my code and testing with multiple different values to see what sort of general shape comes out. 
+* 							They are not by any means accurate, but I feel they give a close enough glipse into the rate of gtowth that I can feel confident writing it.
+* 						|WHILE LOOP ------------------------------------------------------------------------------------------------------------------------------------------------------------|
+* Assigns+Comp	|Assign	|Min (Comp)	|Assign	|Comp	|Assign	(Max)	|Comp^	|Assign (Max)	|Comp+		|Assign (Max)	|Assign (Max)	|j		|Comp*	|Assign x4 (Max)	|Comp	|			|Assign x2	|
+* 2				|1		|1			|1		|1		|1, return		|1		|1				|1			|1, continue+	|1				|n-1	|1		|4, break*			|1		|continue^	|2			|
+* -				|-		|1			|1		|1		|1, return		|1		|1				|1			|1, continue+	|1				|n-2	|1		|4, break*			|1		|continue^	|2			|
+* -				|-		|1			|1		|1		|1, return		|1		|1				|1			|1, continue+	|1				|n-3	|1		|4, break*			|1		|continue^	|2			|
+* -				|-		|1			|1		|1		|1, return		|1		|1				|1			|1, continue+	|1				|1		|1		|4, break*			|1		|continue^	|2			|
+* -				|-		|1			|1		|1		|1, return		|1		|1				|1			|1, continue+	|1				|0		|1		|4, break*			|1		|continue^	|2			|
+* --------------|-------|-----------|-------|-------|---------------|-------|---------------|-----------|---------------|---------------|-------|-------|-------------------|-------|-----------|-----------|
+* 2				|1		|			|		|		|				|		|				|			|
+*
+* Best Case: 2 = 2
+* Worst Case: Approximately 2^n
+* Average Case: 2^n+2/2
+* Big O Notation: O(2^n)
 */
 package Assignment2;
 import java.util.Scanner;
 import java.util.LinkedList;
 
 public class Partition {
-	static int comparisons = 0;
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int input;
@@ -30,7 +84,7 @@ public class Partition {
 			System.out.println("\n-----------------MAIN MENU--------------");
 			System.out.println("1. Read set size (number of integers)");
 			System.out.println("2. Read integer values");
-			System.out.println("3. Run algorithm and display output(substrings and number of comparisons)");
+			System.out.println("3. Run algorithm and display output");
 			System.out.println("4. Exit program");
 			System.out.print("\nEnter option number: ");
 			
@@ -55,7 +109,7 @@ public class Partition {
 				}
 			}
 			else if (input == 3) { //Runs algorithm and displays output
-				System.out.println("\nSet size:\t\t\t" + size);
+				System.out.printf("\nSet size:\t\t\t%d integers\n", size);
 				System.out.print("Integer values:\t\t\t");
 				for (int i : integerValues)
 					System.out.print(i + " ");
@@ -88,7 +142,7 @@ public class Partition {
 		
 		//Check to make sure that the sum of the input values are even.
 		//  If not, we can return immediately without needing the full algorithm.
-		//  Math behind this theory: targetSum = 2x where x equals the sum of one subset, therefore targetSum MUST be even.
+		//  Math behind this: targetSum = 2x where x equals the sum of one subset, therefore targetSum MUST be even.
 		if (targetSum % 1 != 0)
 			return "No disjoint subsets with the same sum of their elements found.";
 		
@@ -108,7 +162,7 @@ public class Partition {
 				}
 				return String.format("%s\n\t\t\t\t%s", subset1.toString(), subset2.toString()).replace('[', '{').replace(']', '}'); //Formatted Output
 			}
-			else if(trackedSum > targetSum) { //If the tracked sum is larger than the target sum
+			if(trackedSum > targetSum) { //If the tracked sum is larger than the target sum
 				trackedSum -= input[i]; //Remove the newly added input from the tracked subset
 				if(i < n-1) { //If this is not the last element in the input, skip it.
 					i++;
