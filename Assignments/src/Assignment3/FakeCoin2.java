a208030d44a6b48329207277fdf0558b0bcb7d05
DesktopCoder1407 <dcoder1407@gmail.com>
2022-03-01 10:56:46 -0500

    A3: Complete Problem 2 Part A

--- Assignments/src/Assignment3/FakeCoin2.java
+++ Assignments/src/Assignment3/FakeCoin2.java
@@ -10,26 +10,48 @@
 *   Develop an algorithm that divides the set by factor of 3 and finds the fake coin.
 *   
 * PART A, PSEUDOCODE:
-* FindFake(A[n]):
+* FindFake(A[n], l, r):
 * //A[n] is an array of n elements containing the weights of the coins
+* //l is the index of the leftmost element
+* //r is the index of the rightmost element
+* //d is the a count of the number of divisions currently taken
 * 
-* if n = 2
-*   if A[0] > A[1]
-*   
+* d <- 0
+* numCoins <- r - l + 1
 * 
+* if numCoins = 1 //If there is only one coin, return the index.
+*   return l
+* if numCoins = 2 //If there are two coins, return the index of the smaller.
+*   if A[l] < A[r]
+*     return l
+*   else
+*     return r
+* 
+* //Set the split locations to determine the first and second thirds of the coins.
+* split1 <- l + (numCoins/3) - 1
+* split2 <- l + 2*(numCoins/3) - 1
 * sumFirstThird <- 0
 * sumSecondThird <- 0
-* for i <- 0 to floor(n/3) - 1 do
-*   sumFirstThird <- sumFirstThird + A[i]
-* for i <- floor(n/3) to 2 * floor(n/3) - 1 do
-*   sumSecondThird <- sumSecondTHird + A[i]
 * 
+* //Sum the First and Second thirds.
+* for i <- l to split1 do
+*   sumFirstThird <- sumFirstThird + A[i]
+* for i <- split1 + 1 to split2 do
+*   sumSecondThird <- sumSecondThird + A[i]
+* 
+* //Recurrence: The third of the array that weights less is re-run through the FindFake function.
 * if sumFirstThird < sumSecondThird
-*   FindFake(A[0..floor(n/3) - 1)
+*   d <- d + 1
+*   FindFake(A, l, split1)
 * else if sumSecondThird < sumFirstThird
-*   FindFake(A[floor(n/3)..2 * floor(n/3) - 1])
+*   d <- d + 1
+*   FindFake(A, split1 + 1, split2)
 * else
-*   FindFake(A[2 * floor(n/3)..n - 1])
+*   d <- d + 1
+*   FindFake(A, split2 + 1, r)
+* 
+* 
+* PART B, RECURRENCE RELATION:
 * 
 */
 package Assignment3;
@@ -73,13 +95,14 @@ public class FakeCoin2 {
 				}
 			}
 			else if (input == 3) { //Runs algorithm comparison and displays results
-				System.out.println("\nArray size:\t" + size);
-				System.out.print("Array values:\t");
+				System.out.println("\nArray size:\t\t" + size);
+				System.out.print("Array values:\t\t");
 				for (int i : coins)
 					System.out.print(i + " ");
-				//run algorithm
-				System.out.println("\n# of divisions:\t" + divisions);
-				System.out.println("Fake coin index:\t");
+				divisions = 0;
+				int fakeCoinIndex = FindFake(coins, 0, coins.length - 1);
+				System.out.println("\n# of divisions:\t\t" + divisions);
+				System.out.println("Fake coin index:\t" + fakeCoinIndex);
 			}
 			else if (input == 4) { //Exits the program
 				break;
@@ -88,5 +111,43 @@ public class FakeCoin2 {
 				System.out.println("Please enter an integer number from 1 to 4.");
 		}
 		scan.close();
+	}
+	
+	static int FindFake(int[] coins, int l, int r) {
+		int numCoins = r - l + 1;
+		
+		if (numCoins == 1) //If there is only one coin, return the index.
+			return l;
+		if (numCoins == 2) { //If there are two coins, return the index of the smaller coin.
+			if (coins[l] < coins[r])
+				return l;
+			else
+				return r;
+		}
+		
+		//Set the splits between the thirds of the coins
+		int split1 = l + (numCoins/3) - 1;
+		int split2 = l + 2*(numCoins/3) - 1;
+		int sumFirstThird = 0;
+		int sumSecondThird = 0;
+		
+		//Sum the first two of the three thirds.
+		for (int i = l; i <= split1; i++)
+			sumFirstThird += coins[i];
+		for (int i = split1 + 1; i <= split2; i++)
+			sumSecondThird += coins[i];
+		
+		if (sumFirstThird < sumSecondThird) { //If the first third is smaller (has the fake) rerun the FindFake algorithm with that section of the array.
+			divisions++;
+			return FindFake(coins, l, split1);
+		}
+		else if (sumSecondThird < sumFirstThird) { //If the second third is smaller (has the fake) rerun the FindFake algorithm with that section of the array.
+			divisions++;
+			return FindFake(coins, split1 + 1, split2);
+		}
+		else { //If the rest of the array is smaller (has the fake) rerun the FindFake algorithm with that section of the array.
+			divisions++;
+			return FindFake(coins, split2 + 1, r);
+		}
 	}
 }
